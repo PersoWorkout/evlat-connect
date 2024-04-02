67dc4352229cbdb22b2ea1e7998a748bee7b3940
Yasin2719 <karakusyasin19@gmail.com>
2024-04-02 13:46:15 +0200

    feat(users): add and test password value object

---
+++ src/Domain/Users/ValueObjects/PasswordValueObject.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Abstract;
+using Domain.Users.Errors;
+using System.Text.RegularExpressions;
+
+namespace Domain.Users.ValueObjects
+{
+    public partial class PasswordValueObject : ValueObject
+    {
+        private const string PasswordRegexPattern =
+            @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[~`!@#\$%\^&\*\(\)_\-\+=\{\[\}\]\|\\:;'<,>\.\?\/]).{8,}$";
+
+        [GeneratedRegex(PasswordRegexPattern, RegexOptions.None, 250)]
+        private static partial Regex MyRegex();
+
+        public string Value { get; set; }
+
+        private PasswordValueObject(string value)
+        {
+            Value = value;
+        }
+
+        public static Result<PasswordValueObject> Create(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return Result<PasswordValueObject>.Failure(
+                    PasswordErrors.Empty);
+
+            if (!MyRegex().IsMatch(value))
+                return Result<PasswordValueObject>.Failure(
+                    PasswordErrors.Invalid);
+                
+
+            return Result<PasswordValueObject>
+                .Success(new PasswordValueObject(value));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
---
+++ tests/Domain.UnitTests/Users/ValueObjects/PasswordValueObjectTest.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Users.Errors;
+using Domain.Users.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UnitTests.Users.ValueObjects
+{
+    public class PasswordValueObjectTest
+    {
+        [Fact]
+        public void Create_ShouldReturnFailure_WhenValueIsEmpty()
+        {
+            //Arrange
+            string value = string.Empty;
+
+            //Act
+            var passwordValue = PasswordValueObject.Create(value); 
+
+            //Assert
+            Assert.True(passwordValue.IsFailure);
+            Assert.Contains(PasswordErrors.Empty, passwordValue.Errors);
+        }
+
+        [Fact]
+        public void Create_ShouldReturnFailure_WhenValueIsNotAValidPassword()
+        {
+            //Arrange
+            const string Value = "invalid";
+
+            //Act
+            var passwordValue = PasswordValueObject.Create(Value);
+
+            //Assert
+            Assert.True(passwordValue.IsFailure);
+            Assert.Contains(PasswordErrors.Invalid, passwordValue.Errors);
+        }
+
+        [Fact]
+        public void Create_ShouldReturnSuccess_WhenValueIsValid()
+        {
+            //Arrange
+            const string Value = "Password123!";
+
+            //Act
+            var passwordValue = PasswordValueObject.Create(Value);
+
+            //Assert
+            Assert.True(passwordValue.IsSucess);
+            Assert.Equal(Value, passwordValue.Data!.Value);
+        }
+    }
+}
