53db8c9c3896d86fd62abad1a6f86725271d939f
Yasin2719 <karakusyasin19@gmail.com>
2024-04-02 15:23:39 +0200

    feat(users): add user repository interface

---
+++ src/Application/Users/IUserRepostory.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Users;
+
+namespace Application.Users
+{
+    public interface IUserRepostory
+    {
+        Task<IEnumerable<User>> GetUsers();
+        Task<IEnumerable<User>> GetStudents();
+        Task<IEnumerable<User>> GetProfessors();
+        Task<User> Create(User user);
+        Task<User> GetUserById(Guid Id);
+        Task<User> GetUserByUsername(string username);
+        Task<string> GetNewUsername(string firstname, string lastname);
+        Task<User> Update(User user);
+        Task<int> Delete(User user);
+    }
+}
